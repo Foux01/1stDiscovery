530ab8d986bd94374b1abecd5807aba7cf8ab702
Foux01 <foux-01@gmail.com>
2015-04-14 11:33:18 +0200

    test 2

---
+++ discoveries/bluetooth/src/main/java/com/alex/morjaret/discovery/bluetooth/BlueToothDiscovery.java
@@ -0,0 +1,11 @@
+package com.alex.morjaret.discovery.bluetooth;
+
+/**
+ * Created by alex on 14/04/15.
+ */
+@Component
+@Provides(specifications = DiscoveryService.class)
+public class BlueToothDiscovery extends AbstractDiscoveryComponent{
+
+
+}
---
+++ discoveries/bluetooth/src/main/java/com/alex/morjaret/discovery/bluetooth/BlueToothDiscovery2.java
@@ -0,0 +1,7 @@
+package com.alex.morjaret.discovery.bluetooth;
+
+/**
+ * Created by alex on 14/04/15.
+ */
+public class BlueToothDiscovery2 {
+}
